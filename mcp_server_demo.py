eadd93e9e6c82d090e7c22a0cb0f049f0957e91a
SRISWETHA RAVINDRAN <126751897+Sweee2012@users.noreply.github.com>
2025-05-22 10:14:03 +0530

    Add files via upload

---
+++ mcp_server_demo.py
@@ -0,0 +1,43 @@
+import httpx
+from mcp.server.fastmcp import FastMCP
+from bs4 import BeautifulSoup
+
+
+mcp = FastMCP(
+    'Your MCP Tools',
+    dependencies=['beautifulsoup4']
+)
+
+@mcp.tool(
+        name='Extract-Web-Page-Content-Tool',
+        description='Tool to extract page content in text format'
+)
+
+def extract_web_content(url: str) -> str | None:
+    """
+    Extract text content from a web page.
+
+    Args:
+        url: URL of the web page to extract content from.
+
+    Returns:
+        str: Extracted text content from the web page.
+    """
+    try:
+        response = httpx.get(
+            url,
+            headers={
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:136.0) Gecko/20100101 Firefox/136.0'
+            },
+            timeout=10.0,
+            follow_redirects=True
+        )
+        response.raise_for_status()
+        soup = BeautifulSoup(response.text, 'html.parser')
+        content = soup.get_text().replace('\n', ' ').replace('\r', ' ').strip()
+        return content
+    except Exception as e:
+        return f'Error fetching content: {str(e)}'
+    
+if __name__ == "__main__":
+    mcp.run(transport='stdio')
